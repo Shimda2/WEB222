32ae42cbfabbd785fb786eb3e726948f328d9e25
Shimda2 <87587749+Shimda2@users.noreply.github.com>
2022-04-11 23:27:08 -0400

    Add files via upload

--- Final Project/product.js
+++ Final Project/product.js
@@ -1,6 +1,34 @@
 /* Sorry professor, I was not able to finish this part as I ran out of time*/
 
+function ValidateInput() {
+    if ((ValidateInput == true) && (ValidatePrice == true) && (ValidatePrice==true) && (ValidateSupplier ==true) && (ValidateEmail == true) && (validateCheckbox() == true))
+        return true;
+    else return false;
+  }
 
-function formValidation() {
-    return true;
+function ValidateID() {
+    var prodID = document.productID.value.trim()
  }
+
+ function ValidateDescription()
+ {
+ }
+
+ function ValidatePrice()
+ {
+
+ }
+
+ function ValidateSupplier(){
+
+ }
+
+ function ValidateEmail() {
+
+ }
+
+ function validateCheckbox() {
+
+ }
+
+
